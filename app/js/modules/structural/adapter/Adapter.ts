88532410a937c91c71f1924fd5d0206f32e49d55
Gaina Dan <danugaina1403@gmail.com>
2022-12-22 10:26:47 +0200

    [feat] add Adapter example

--- app/js/modules/structural/adapter/Adapter.ts
+++ app/js/modules/structural/adapter/Adapter.ts
@@ -2,6 +2,45 @@ import { IPattern } from '../../../interfaces/IPattern';
 
 export default class Adapter implements IPattern {
     public main(): void {
-        console.log('Adapter');
+        this.runClient(new Client());
+        this.runClient(new LegacyClientAdapter(new LegacyClient()));
+    }
+
+    // The client code works with a "Client" interface.
+    // The adapter will extend the "Client" and utilize a 3rd party service.
+    // The adapter is used when this "Client" interface cannot be modified but
+    // we need to somehow integrate some other code/serivce.
+    private runClient(client: Client): void {
+        console.log('Data: ', client.getData());
+    }
+}
+
+class Client {
+    public getData(): string {
+        return 'normal data';
+    }
+}
+
+class LegacyClient {
+    public getFirstPart(): string {
+        return 'legacy';
+    }
+
+    public getSecondPart(): string {
+        return 'data';
+    }
+}
+
+class LegacyClientAdapter extends Client {
+    private _legacyClient: LegacyClient;
+
+    constructor(legacyClient: LegacyClient) {
+        super();
+        this._legacyClient = legacyClient;
+    }
+
+    // the Client's modified method which will utilize the adaptee
+    public getData(): string {
+        return `${this._legacyClient.getFirstPart()} ${this._legacyClient.getSecondPart()}`;
     }
 }
